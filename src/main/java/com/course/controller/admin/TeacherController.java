110a5dda1bfbbbd646e6f30c26d2b2c401c722d2
Alpple <3219257078@qq.com>
2025-01-27 23:23:55 +0800

    #FEAT:教师管理功能模块

--- src/main/java/com/course/controller/admin/TeacherController.java
+++ src/main/java/com/course/controller/admin/TeacherController.java
@@ -2,12 +2,60 @@ package com.course.controller.admin;
 
 import com.course.config.themis.annotation.Admin;
 import com.course.controller.BaseController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.course.model.entity.TeacherEntity;
+import com.course.model.vo.response.ResultVO;
+import com.course.service.admin.TeacherService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Admin(Admin.TEACHER_MANAGE)
 @RequestMapping("/admin/teacher")
 @RestController
 public class TeacherController extends BaseController {
+    private final TeacherService service;
 
+    public TeacherController(TeacherService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public ResultVO get(@PathVariable Integer id) {
+        return service.get(id);
+    }
+
+    @PostMapping
+    public ResultVO create(@RequestBody @Validated TeacherEntity entity) {
+        return service.create(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResultVO delete(@PathVariable Integer id) {
+        return service.delete(id);
+    }
+
+    @PutMapping
+    public ResultVO update(@RequestBody @Validated TeacherEntity entity) {
+        return service.update(entity);
+    }
+
+    @RequestMapping("/page/count")
+    public ResultVO getPageCount(String departmentName, String name) {
+        return service.getPageCount(departmentName, name);
+    }
+
+    @RequestMapping("/page")
+    public ResultVO getPage(String departmentName, String name) {
+        return service.getPage(1, departmentName, name);
+    }
+
+    @RequestMapping("/page/{index}")
+    public ResultVO getPage(@PathVariable Integer index, String departmentName, String name) {
+        return service.getPage(index, departmentName, name);
+    }
+
+    @Admin
+    @RequestMapping("/names")
+    public ResultVO listName() {
+        return service.listName();
+    }
 }
--- src/main/java/com/course/manager/admin/TeacherManager.java
+++ src/main/java/com/course/manager/admin/TeacherManager.java
@@ -1,9 +1,70 @@
 package com.course.manager.admin;
 
+import com.course.dao.CourseDAO;
+import com.course.dao.DepartmentDAO;
+import com.course.dao.TeacherDAO;
 import com.course.manager.BaseManager;
+import com.course.model.entity.DepartmentEntity;
+import com.course.model.entity.TeacherEntity;
+import com.course.model.vo.response.IdNameVO;
+import com.course.model.vo.response.table.TeacherItemVO;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TeacherManager extends BaseManager {
+    private final DepartmentDAO departmentDAO;
+    private final TeacherDAO teacherDAO;
+    private final CourseDAO courseDAO;
 
+    public TeacherManager(DepartmentDAO departmentDAO, TeacherDAO teacherDAO, CourseDAO courseDAO) {
+        this.departmentDAO = departmentDAO;
+        this.teacherDAO = teacherDAO;
+        this.courseDAO = courseDAO;
+    }
+
+    public Integer getPageCount(String departmentName, String name) {
+        int count = teacherDAO.count(departmentName, name);
+        return calcPageCount(count, TeacherDAO.PAGE_SIZE);
+    }
+
+    public List<TeacherItemVO> getPage(Integer index, String departmentName, String name) {
+        return teacherDAO.getPage(index, departmentName, name);
+    }
+
+    public TeacherEntity get(Integer id) {
+        return teacherDAO.get(id);
+    }
+
+    public int create(TeacherEntity entity) {
+        return teacherDAO.insert(entity);
+    }
+
+    public int update(TeacherEntity entity) {
+        return teacherDAO.update(entity);
+    }
+
+    public int delete(Integer id) {
+        return teacherDAO.delete(id);
+    }
+
+    public boolean hasCourse(Integer teacherId) {
+        return courseDAO.countByTeacherId(teacherId) > 0;
+    }
+
+    public DepartmentEntity getDepartmentById(Integer id) {
+        return departmentDAO.get(id);
+    }
+
+    public List<IdNameVO> listName() {
+        List<IdNameVO> voList = new ArrayList<>();
+        List<TeacherEntity> entityList = teacherDAO.listName();
+        for (TeacherEntity entity : entityList) {
+            voList.add(new IdNameVO(entity.getId(), entity.getName()));
+        }
+
+        return voList;
+    }
 }
